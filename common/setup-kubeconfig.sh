3fd303e1e32a9b040dcff21245e835dbb31ce67f
Matthew Clarke <matthew.clarke@ft.com>
2017-07-04 17:13:55 +0100

    fix setup script

--- common/setup-kubeconfig.sh
+++ common/setup-kubeconfig.sh
@@ -2,22 +2,22 @@
 
 mkdir -p ~/.kube
 
-mkdir -p .circle/eu-test
-mkdir -p .circle/us-test
-mkdir -p .circle/eu-prod
-mkdir -p .circle/us-prod
+mkdir -p common/eu-test
+mkdir -p common/us-test
+mkdir -p common/eu-prod
+mkdir -p common/us-prod
 
-echo -e "$TEST_EU_CA_PEM" > .circle/eu-test/ca.pem
-echo -e "$TEST_US_CA_PEM" > .circle/us-test/ca.pem
-echo -e "$PROD_EU_CA_PEM" > .circle/eu-prod/ca.pem
-echo -e "$PROD_US_CA_PEM" > .circle/us-prod/ca.pem
+echo -e "$TEST_EU_CA_PEM" > common/eu-test/ca.pem
+echo -e "$TEST_US_CA_PEM" > common/us-test/ca.pem
+echo -e "$PROD_EU_CA_PEM" > common/eu-prod/ca.pem
+echo -e "$PROD_US_CA_PEM" > common/us-prod/ca.pem
 
-echo -e "$TEST_EU_ADMIN_PEM" > .circle/eu-test/admin.pem
-echo -e "$TEST_US_ADMIN_PEM" > .circle/us-test/admin.pem
-echo -e "$PROD_EU_ADMIN_PEM" > .circle/eu-prod/admin.pem
-echo -e "$PROD_US_ADMIN_PEM" > .circle/us-prod/admin.pem
+echo -e "$TEST_EU_ADMIN_PEM" > common/eu-test/admin.pem
+echo -e "$TEST_US_ADMIN_PEM" > common/us-test/admin.pem
+echo -e "$PROD_EU_ADMIN_PEM" > common/eu-prod/admin.pem
+echo -e "$PROD_US_ADMIN_PEM" > common/us-prod/admin.pem
 
-echo -e "$TEST_EU_ADMIN_KEY_PEM" > .circle/eu-test/admin-key.pem
-echo -e "$TEST_US_ADMIN_KEY_PEM" > .circle/us-test/admin-key.pem
-echo -e "$PROD_EU_ADMIN_KEY_PEM" > .circle/eu-prod/admin-key.pem
-echo -e "$PROD_US_ADMIN_KEY_PEM" > .circle/us-prod/admin-key.pem
+echo -e "$TEST_EU_ADMIN_KEY_PEM" > common/eu-test/admin-key.pem
+echo -e "$TEST_US_ADMIN_KEY_PEM" > common/us-test/admin-key.pem
+echo -e "$PROD_EU_ADMIN_KEY_PEM" > common/eu-prod/admin-key.pem
+echo -e "$PROD_US_ADMIN_KEY_PEM" > common/us-prod/admin-key.pem
