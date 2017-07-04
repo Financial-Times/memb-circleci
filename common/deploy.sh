24a89dd75f449f9ce4845027c7087afecd0578f5
Matthew Clarke <matthew.clarke@ft.com>
2017-07-04 17:08:20 +0100

    correct folder

--- common/deploy.sh
+++ common/deploy.sh
@@ -15,16 +15,16 @@ echo "ENV=$ENV"
 echo "NEW_VERSION=$NEW_VERSION"
 
 # switches the active context in kubeconfig to be the same as the ENV provided
-sudo kubectl config use-context $SYSTEM_CODE-context --kubeconfig .circle/kubeconfig
+sudo kubectl config use-context $SYSTEM_CODE-context --kubeconfig common/kubeconfig
 
 # applies the kubernetes files in the common kubernetes folder
-kubectl apply -f kubernetes/env/$SYSTEM_CODE/ --kubeconfig .circle/kubeconfig
+kubectl apply -f kubernetes/env/$SYSTEM_CODE/ --kubeconfig common/kubeconfig
 
 # applies the kubernetes files in the env specific kubernetes folder
-kubectl apply -f kubernetes/ --kubeconfig .circle/kubeconfig
+kubectl apply -f kubernetes/ --kubeconfig common/kubeconfig
 
 # sets the current image to be used TODO support sidecar containers
-kubectl set image deployment/SYSTEM_CODE SYSTEM_CODE=$NEW_VERSION --kubeconfig .circle/kubeconfig
+kubectl set image deployment/SYSTEM_CODE SYSTEM_CODE=$NEW_VERSION --kubeconfig common/kubeconfig
 
 # tracks the status of the deployment
-kubectl rollout status deployment/SYSTEM_CODE --kubeconfig .circle/kubeconfig
+kubectl rollout status deployment/SYSTEM_CODE --kubeconfig common/kubeconfig
